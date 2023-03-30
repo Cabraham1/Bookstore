2f29f89df853afb218c2cffeb4df527816c5809d
cabraham1 <christopherabraham8@gmail.com>
2023-03-30 21:56:53 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import BookLists from './components/BookLists';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <div className="App" />
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<BookLists />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
