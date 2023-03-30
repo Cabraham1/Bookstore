8b5850fc6bdfdf2f964d856d3c3f35348c8dad9b
cabraham1 <christopherabraham8@gmail.com>
2023-03-30 22:19:44 +0100

    Add books to Ui

--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
+const books = [
+  {
+    title: 'Invaluabe',
+    author: 'Maya Grossman',
+    id: 1,
+  },
+  {
+    title: 'Turn the ship Around',
+    author: 'L. David Marquet',
+    id: 2,
+  },
+  {
+    title: 'Dare to Lead,',
+    author: 'Brené Brown',
+    id: 3,
+  },
+  {
+    title: 'Invaluabe',
+    author: 'Maya Grossman',
+    id: 4,
+  },
+];
+
 const BookLists = () => (
-  <div>BookLists</div>
+  <div>
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>
+          <span>
+            {book.title}
+            {' '}
+            -
+            {book.author}
+          </span>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default BookLists;
