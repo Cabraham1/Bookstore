e163b9e7a4ef96407e986fa40d63ea0b15fff9ba
cabraham1 <christopherabraham8@gmail.com>
2023-03-31 00:12:37 +0100

    Populate books

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,8 +4,8 @@ const AddBook = () => (
   <div>
     <h1>ADD NEW BOOK</h1>
     <form>
-      <input type="text" required value="" placeholder="Book title" />
-      <input type="text" required value="" placeholder="Book author" />
+      <input type="text" required placeholder="Book title" />
+      <input type="text" required placeholder="Book author" />
       <button type="submit">ADD BOOK</button>
     </form>
   </div>
--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,45 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddBook from './AddBook';
+import Book from './Book';
 
-const books = [
-  {
-    title: 'Invaluabe',
-    author: 'Maya Grossman',
-    id: 1,
-  },
-  {
-    title: 'Turn the ship Around',
-    author: 'L. David Marquet',
-    id: 2,
-  },
-  {
-    title: 'Dare to Lead,',
-    author: 'Brené Brown',
-    id: 3,
-  },
-  {
-    title: 'Invaluabe',
-    author: 'Maya Grossman',
-    id: 4,
-  },
-];
-
-const BookLists = () => (
-  <div>
-    <ul>
+const BookLists = () => {
+  const [books] = useState([
+    {
+      title: 'Invaluabe',
+      author: 'Maya Grossman',
+      id: 1,
+    },
+    {
+      title: 'Turn the ship Around',
+      author: 'L. David Marquet',
+      id: 2,
+    },
+    {
+      title: 'Dare to Lead,',
+      author: 'Brené Brown',
+      id: 3,
+    },
+    {
+      title: 'Invaluabe',
+      author: 'Maya Grossman',
+      id: 4,
+    },
+  ]);
+  return (
+    <div>
       {books.map((book) => (
-        <li key={book.id}>
-          <span>
-            {book.title}
-            {' '}
-            -
-            {book.author}
-          </span>
-        </li>
+        <Book key={book.id} title={book.title} author={book.author} />
       ))}
-    </ul>
-    <AddBook />
-  </div>
-);
+      <AddBook />
+    </div>
+  );
+};
 
 export default BookLists;
