6a125a393acb9f998486b63ed97df52bfe2bcf71
cabraham1 <christopherabraham8@gmail.com>
2023-04-05 11:57:37 +0100

    implement the remove functionality usiing dispatch

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/Books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const { title, author, id } = props;
+  const dispatch = useDispatch();
+
+  const handleRemoveClick = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <div>
       <ul>
         <li>{title}</li>
         <li>{author}</li>
-        <button type="button">Remove</button>
+        <button type="button" onClick={handleRemoveClick}>
+          Remove
+        </button>
       </ul>
     </div>
   );
 };
+
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
