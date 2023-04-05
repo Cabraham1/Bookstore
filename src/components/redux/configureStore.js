ba3f3c006bc5b718f1f9df3c6b3c224783d22b7a
cabraham1 <christopherabraham8@gmail.com>
2023-04-05 08:37:47 +0100

    setup redux store

---
+++ src/components/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import books from './books/Books';
+import categoriesReducer from './categories/Categories';
+
+const rootReducer = combineReducers({
+  books,
+  categories: categoriesReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
