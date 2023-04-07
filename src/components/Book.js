6207ede297a2668367bf4b7510badf7cac4a735a
cabraham1 <christopherabraham8@gmail.com>
2023-04-07 12:57:51 +0100

    Update the book components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,33 +1,71 @@
-import React from 'react';
-import { PropTypes } from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/Books';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { PropTypes } from "prop-types";
+import { removeBook } from "../redux/books/Books";
+import { ProgressBar, percentage } from "./ProgressBar";
 
 const Book = (props) => {
-  const { title, author, id } = props;
-  const dispatch = useDispatch();
+  const dispath = useDispatch();
+  const { title, author, id, category } = props;
 
-  const handleRemoveClick = () => {
-    dispatch(removeBook(id));
+  const removeHandler = () => {
+    dispath(removeBook(id));
   };
 
   return (
-    <div>
-      <ul>
-        <li>{title}</li>
-        <li>{author}</li>
-        <button type="button" onClick={handleRemoveClick}>
-          Remove
-        </button>
-      </ul>
-    </div>
+    <>
+      <div className="book">
+        <div className="book-desc">
+          <div className="book-obj">
+            <p className="category">{category}</p>
+            <h1 className="title">{title}</h1>
+            <small className="author">{author}</small>
+          </div>
+          <div className="obj-actions">
+            <button type="button" className="obj-btn">
+              Comment
+            </button>
+            |
+            <button type="button" className="obj-btn" onClick={removeHandler}>
+              Remove
+            </button>
+            |
+            <button type="button" className="obj-btn">
+              Edit
+            </button>
+          </div>
+        </div>
+        <div className="progress-chapter">
+          <div className="progress">
+            <div>
+              <ProgressBar />
+            </div>
+            <div>
+              <div className="percent">{`${percentage}%`}</div>
+              <div className="completed">completed</div>
+            </div>
+          </div>
+          <div className="pipe" />
+          <div>
+            <div className="current-chapter">Current Chapter</div>
+            <div className="chapter">
+              <span className="capterXOfY">Chapter 5</span>
+            </div>
+            <button className="update-button" type="button">
+              Update Progress
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
